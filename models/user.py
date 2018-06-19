def8a256d6e7e4918a7edf8c96a2e7f1acc5a611
mickeyd <dafty_@hotmail.com>
2018-06-19 22:00:47 +0100

    gradual start of models... no data added yet.

--- models/user.py
+++ models/user.py
@@ -1,5 +1,6 @@
 import mongoengine
 
+<<<<<<< HEAD
 class User(mongoengine.Document):
     name = mongoengine.StringField(required=True,max_length=50)
     position = mongoengine.StringField(required=True)
@@ -8,3 +9,7 @@ class User(mongoengine.Document):
     email = mongoengine.EmailField()
     department = mongoengine.StringField(max_length=20)
     company = mongoengine.StringField(max_length=20)
+=======
+class User(mongoengine.EmbeddedDocument):
+    name = mongoengine.StringField
+>>>>>>> gradual start of models... no data added yet.
