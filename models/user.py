593b0775993c996ab9575e0c343af71d16eea8b6
michaeldixon-tfm <michael.dixon@tfm-uk.com>
2018-06-20 17:12:23 +0100

    parser included that works

--- models/user.py
+++ models/user.py
@@ -1,4 +1,10 @@
 import mongoengine
 
-class User(mongoengine.EmbeddedDocument):
-    name = mongoengine.StringField
+class User(mongoengine.Document):
+    name = mongoengine.StringField(required=True,max_length=50)
+    position = mongoengine.StringField(required=True)
+    extension = mongoengine.IntField()
+    telephone = mongoengine.ListField()
+    email = mongoengine.EmailField()
+    department = mongoengine.StringField(max_length=20)
+    company = mongoengine.StringField(max_length=20)
